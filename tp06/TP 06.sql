07ec8edb1e841130c95e621a1cbcda177f5d0a8b
Geoffdl <geoffroydelorme@gmail.com>
2025-04-10 16:08:21 +0200

    tp06

--- tp06/TP 06.sql
+++ tp06/TP 06.sql
@@ -1,2 +1,26 @@
+SET SQL_SAFE_UPDATES = 0;
+
 -- a. Supprimer dans la table compo toutes les lignes concernant les bons de commande d'avril 2019
+START TRANSACTION;
+
+DELETE c.*
+FROM COMPO c
+         JOIN BON b ON c.ID_BON = b.ID
+WHERE MONTH(b.DATE_CMDE) = 4
+  AND YEAR(b.DATE_CMDE) = 2019;
+
+
 -- b. Supprimer dans la table bon tous les bons de commande d'avril 2019.
+START TRANSACTION;
+DELETE c.* -- Child first
+FROM COMPO c
+         JOIN BON b ON c.ID_BON = b.ID
+WHERE MONTH(b.DATE_CMDE) = 4
+  AND YEAR(b.DATE_CMDE) = 2019;
+
+DELETE b.* -- Parent
+FROM BON b
+WHERE MONTH(b.DATE_CMDE) = 4
+  AND YEAR(b.DATE_CMDE) = 2019;
+
+SET SQL_SAFE_UPDATES = 1;
